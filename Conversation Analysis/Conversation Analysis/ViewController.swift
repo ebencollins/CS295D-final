bba131ef1438be83a50153fc384553a53b11e1cf
Eben Collins <collinseben@gmail.com>
2020-02-11 11:09:57 -0500

    initial base project

---
+++ Conversation Analysis/Conversation Analysis/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Conversation Analysis
+//
+//  Created by Eben Collins on 2020-2-11.
+//  Copyright © 2020 conversation-analysis. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Conversation Analysis/Conversation AnalysisTests/Conversation_AnalysisTests.swift
@@ -0,0 +1,34 @@
+//
+//  Conversation_AnalysisTests.swift
+//  Conversation AnalysisTests
+//
+//  Created by Eben Collins on 2020-2-11.
+//  Copyright © 2020 conversation-analysis. All rights reserved.
+//
+
+import XCTest
+@testable import Conversation_Analysis
+
+class Conversation_AnalysisTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
