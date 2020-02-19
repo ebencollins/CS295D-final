904e9b40ee47ab4c818212798543a38469a9cc9f
devon66h <devon66h@gmail.com>
2020-02-18 20:45:33 -0500

    added UITabBarController that switches between RecordingView and ListView still working on images

---
+++ Conversation Analysis/Conversation Analysis/ListViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ListViewController.swift
+//  Conversation Analysis
+//
+//  Created by devon on 2/18/20.
+//  Copyright © 2020 conversation-analysis. All rights reserved.
+//
+
+import UIKit
+
+class ListViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // print to console to alert whether it's view has been loaded
+        print("ListViewController loaded its view")
+    }
+    
+}
---
+++ Conversation Analysis/Conversation Analysis/RecordingViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RecordingViewController.swift
+//  Conversation Analysis
+//
+//  Created by devon on 2/18/20.
+//  Copyright © 2020 conversation-analysis. All rights reserved.
+//
+
+import UIKit
+
+class RecordingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // print to console to alert whether it's view has been loaded
+        print("RecordingViewController loaded its view")
+    }
+    
+}
