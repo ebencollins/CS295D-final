f3313c62f3c23e0b4227c5710e787fed97239da4
devon66h <devon66h@gmail.com>
2020-02-19 16:46:05 -0500

    updated UI, still trying to figure out how to resize tab bar items

---
+++ Conversation Analysis/Conversation Analysis/UITabBarController.swift
@@ -0,0 +1,20 @@
+//
+//  UITabBarController.swift
+//  Conversation Analysis
+//
+//  Created by devon on 2/19/20.
+//  Copyright © 2020 conversation-analysis. All rights reserved.
+//
+
+import UIKit
+
+class UITabBarController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // print to console to alert that view has been loaded
+        print("UITabBarController has loaded")
+    }
+    
+}
