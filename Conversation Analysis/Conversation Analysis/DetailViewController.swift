3fd0409dde59ec8868ef27479a8e1b25b6181b99
devon66h <devon66h@gmail.com>
2020-02-21 13:02:59 -0500

    made changes to deatil view and added proecessing view, will need to connect them once we get all the peices

---
+++ Conversation Analysis/Conversation Analysis/DetailViewController.swift
@@ -0,0 +1,25 @@
+//
+//  DetailViewController.swift
+//  Conversation Analysis
+//
+//  Created by devon on 2/20/20.
+//  Copyright © 2020 conversation-analysis. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+    
+    // run when a view is loaded for the first time    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // print to console to alert that view has been loaded
+        print("DetailViewController loaded its view (first time)")
+    }
+    
+    // run everytime a view is loaded
+    override func viewDidAppear(_ animated: Bool) {
+        print("DetailViewController loaded")
+    }
+    
+}
