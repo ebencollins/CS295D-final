8dc0ceaf6135d2fae60f37de21d98d0e78987248
eobarobe <46732661+eobarobe@users.noreply.github.com>
2020-03-21 18:48:18 -0400

    Update ListViewController.swift
    Manually inputting changes due to working on master on my device by accident

--- Conversation Analysis/Conversation Analysis/ListViewController.swift
+++ Conversation Analysis/Conversation Analysis/ListViewController.swift
@@ -8,20 +8,41 @@
 
 import UIKit
 
-class ListViewController: UIViewController {
+class ItemsViewController: UITableViewController {
+    var itemStore: ItemStore!
     
-    // run when a view is loaded for the first time
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int)->Int{
+        return itemStore.allItems.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell{
+        //get a new or recyced cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: "UITableViewCell",for: indexPath)
+        
+        //set the test on the cell with the description of the item
+        //that is at the nth index of items, where n = row this cell
+        //will appear in on the tableview
+        let item = itemStore.allItems[indexPath.row]
+        
+        cell.textLabel?.text = item.name
+        cell.detailTextLabel?.text = "\(item.dateCreated)"
+        
+        
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        print("ListViewController loaded (first time)")
+
+        // print to console to alert whether it's view has been loaded
+        print("ListViewController loaded its view")
     }
-    
-    // run everytime a view is loaded
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        
-        print("ListViewController loaded")
-    }
+//
+//}
+//class Item: NSObject{
+//    var image: String?// Graph
+//    var timeStamp: String?//TimeStamp
+}
+
     
 }
