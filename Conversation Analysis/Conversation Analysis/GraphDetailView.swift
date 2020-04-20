a84feb839e13516d650f903911dcf99c56bbef22
devon66h <devon66h@gmail.com>
2020-04-19 20:44:01 -0400

    added scroll view to GraphDetailView (still needs work)

--- Conversation Analysis/Conversation Analysis/GraphDetailView.swift
+++ Conversation Analysis/Conversation Analysis/GraphDetailView.swift
@@ -1,39 +1,46 @@
-
 import UIKit
 import CoreData
 
-
 class DetailGraphView: UIViewController{
-    //PLACE HOLDER
+    @IBOutlet var scrollView: UIScrollView!
     @IBOutlet var name: UILabel!
-    @IBOutlet var timeStamp: UILabel!
-    @IBOutlet var imageView: UIImageView!
     
     var conversation: Conversation!
 
-
-    
     override func viewWillAppear(_ animated: Bool) { super.viewWillAppear(animated)
-        //place holder for graphs
-        let segments = conversation.getSegments()
+        // set scroll view content size
+        scrollView.contentSize = self.view.frame.size
+        
+        // set name of conversation
         name.text = "\(conversation.hashValue)"
-        timeStamp.text = "\(conversation.duration)"
-        if let segment = segments.first {
-            // Configure Cell
-            DispatchQueue.global().async{
-                if let data = try? Data(contentsOf: segment.image){
-                    if let image = UIImage(data: data){
-                        DispatchQueue.main.async{
-                            self.imageView.image = image
-                        }
-                    }
+        
+        // iterate through all segments and append to scroll view
+        for segment in conversation.getSegments() {
+            // add image to view
+            DispatchQueue.main.async {
+                if let imageData = try? Data(contentsOf: segment.image) {
+                    let imageView = UIImageView(image: UIImage(data: imageData))
+                    imageView.frame = CGRect(x: 0, y: 0, width: 300, height: 150)
+                    self.scrollView.addSubview(imageView)
                 }
             }
         }
+        
+        // place holder for graphs
+//        let segments = conversation.getSegments()
+//        if let segment = segments.first {
+//            // Configure Cell
+//            DispatchQueue.global().async{
+//                if let data = try? Data(contentsOf: segment.image){
+//                    if let image = UIImage(data: data){
+//                        DispatchQueue.main.async{
+//                            self.imageView.image = image
+//                        }
+//                    }
+//                }
+//            }
+//        }
 //        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "< Back", style: .plain, target: self, action: #selector(backAction))
 //
-
     }
-    
 }
-
