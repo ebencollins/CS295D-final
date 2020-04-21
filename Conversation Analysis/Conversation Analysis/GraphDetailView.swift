bbeb3cda439a62e626312579c7e18fa08738b8a2
devon66h <devon66h@gmail.com>
2020-04-21 10:37:17 -0400

    graph detail view now shows all segments

--- Conversation Analysis/Conversation Analysis/GraphDetailView.swift
+++ Conversation Analysis/Conversation Analysis/GraphDetailView.swift
@@ -2,46 +2,35 @@ import UIKit
 import CoreData
 import NotificationBannerSwift
 
-class DetailGraphView: UIViewController{
+class DetailGraphView: UIViewController {
     @IBOutlet var contentView: UIView!
     @IBOutlet var name: UILabel!
     
     var conversation: Conversation!
     
-    override func viewWillAppear(_ animated: Bool) { super.viewWillAppear(animated)
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
         // set name of conversation
         name.text = "\(conversation.hashValue)"
         
         // iterate through all segments and append to scroll view
-        var n = 0
+        var y = 0
         for segment in conversation.getSegments() {
-            // add image to view
+            // get the segments image data
+            let imageData = (try? Data(contentsOf: segment.image))!
             DispatchQueue.main.async {
-                if let imageData = try? Data(contentsOf: segment.image) {
-                    let imageView = UIImageView(image: UIImage(data: imageData))
-                    imageView.frame = CGRect(x: 0, y: n, width: 300, height: 150)
-                    self.contentView.addSubview(imageView)
-                }
+                // create image view w/ imageData
+                let imageView = UIImageView(image: UIImage(data: imageData))
+                imageView.frame = CGRect(x: 0, y: y, width: 300, height: 150)
+                
+                // add image to ui view
+                self.contentView.addSubview(imageView)
+                
+                // increment y value
+                y += 150
             }
-            n += 50
         }
-        
-        // place holder for graphs
-        //        let segments = conversation.getSegments()
-        //        if let segment = segments.first {
-        //            // Configure Cell
-        //            DispatchQueue.global().async{
-        //                if let data = try? Data(contentsOf: segment.image){
-        //                    if let image = UIImage(data: data){
-        //                        DispatchQueue.main.async{
-        //                            self.imageView.image = image
-        //                        }
-        //                    }
-        //                }
-        //            }
-        //        }
-        //        navigationItem.leftBarButtonItem = UIBarButtonItem(title: "< Back", style: .plain, target: self, action: #selector(backAction))
-        //
     }
     
     override func viewDidLoad() {
